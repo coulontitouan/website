4d02cc577220425c8e011b3c5cafc6fa98532a33
CoulonTitouan <coulontitouann@gmail.com>
2023-12-11 01:50:30 +0100

    indent

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    function changeOpacity(){
+    function changeOpacity() {
         let html = document.getElementsByTagName('html').item(0)
         html.style.opacity = 1
     }
-    setTimeout(changeOpacity,500)
+    setTimeout(changeOpacity, 500)
 }, false);
 
 
 let i = 0;
 let elements = document.querySelector("#title").children;
-let textsList = Array.from(elements).map(function(element) {
+let textsList = Array.from(elements).map(function (element) {
     return element.textContent || element.innerText;
 });
-
+let currentElement = 0;
 
 function typeWriter() {
     if (i < textsList[currentElement].length) {
@@ -23,12 +23,10 @@ function typeWriter() {
     } else {
         currentElement++;
         if (currentElement < elements.length) {
-      setTimeout(startTyping, 500);
+            setTimeout(startTyping, 500);
+        }
     }
 }
-}
-
-let currentElement = 0;
 
 function startTyping() {
     i = 0;
@@ -36,23 +34,23 @@ function startTyping() {
 }
 
 
-if ("ontouchstart" in document.documentElement || window.matchMedia("(pointer:none), (pointer:coarse)").matches){
+if ("ontouchstart" in document.documentElement || window.matchMedia("(pointer:none), (pointer:coarse)").matches) {
     document.getElementById("title").classList.add("mobile");
-    Array.from(elements).forEach(function(element){
+    Array.from(elements).forEach(function (element) {
         element.innerHTML = "<br>";
     })
     setTimeout(startTyping, 1500);
 }
 
 // Ce morceau de code n'est jamais éxecuté mais je prefere le garder ici
-if(0==1){
+if (0 == 1) {
     let logos = document.getElementsByTagName("object");
-    for(let logo of logos){
-        logo.addEventListener('load', function() {
-            for(let item of logo.contentWindow.document.getElementsByTagName('path')){
-                    item.style.fill = "snow"
-                }
+    for (let logo of logos) {
+        logo.addEventListener('load', function () {
+            for (let item of logo.contentWindow.document.getElementsByTagName('path')) {
+                item.style.fill = "snow"
             }
+        }
         )
     }
 }
